a2ff38931bf066ee19db4fcf18ce92268f9efdbf
Naor <bdnaor@gmail.com>
2019-08-09 14:54:25 +0300

    using wix nevigaton add auth screen

--- android/app/src/main/java/com/max_rn_course/MainApplication.java
+++ android/app/src/main/java/com/max_rn_course/MainApplication.java
@@ -2,7 +2,7 @@ package com.max_rn_course;
 
 import android.app.Application;
 
-import com.facebook.react.ReactApplication;
+// import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -12,36 +12,58 @@ import com.oblador.vectoricons.VectorIconsPackage;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
+import com.reactnativenavigation.NavigationApplication;
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-    @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
-    }
+public class MainApplication extends NavigationApplication {
 
-    @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
-          , new VectorIconsPackage()
-      );
-    }
+	@Override
+	public boolean isDebug() {
+		// Make sure you are using BuildConfig from your own application
+		return BuildConfig.DEBUG;
+	}
 
-    @Override
-    protected String getJSMainModuleName() {
-      return "index";
-    }
-  };
+	protected List<ReactPackage> getPackages() {
+		// Add additional packages you require here
+		// No need to add RnnPackage and MainReactPackage
+		return Arrays.<ReactPackage>asList(
+			// eg. new VectorIconsPackage()
+      new VectorIconsPackage()
+		);
+	}
 
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
-  }
+	@Override
+	public List<ReactPackage> createAdditionalReactPackages() {
+		return getPackages();
+	}
 
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
-  }
+  // private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+  //   @Override
+  //   public boolean getUseDeveloperSupport() {
+  //     return BuildConfig.DEBUG;
+  //   }
+
+  //   @Override
+  //   protected List<ReactPackage> getPackages() {
+  //     return Arrays.<ReactPackage>asList(
+  //         new MainReactPackage()
+  //         , new VectorIconsPackage()
+  //     );
+  //   }
+
+  //   @Override
+  //   protected String getJSMainModuleName() {
+  //     return "index";
+  //   }
+  // };
+
+  // @Override
+  // public ReactNativeHost getReactNativeHost() {
+  //   return mReactNativeHost;
+  // }
+
+  // @Override
+  // public void onCreate() {
+  //   super.onCreate();
+  //   SoLoader.init(this, /* native exopackage */ false);
+  // }
 }
