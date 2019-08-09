130cdd137849c812c125c0fe3bd3dbb8bb6ae187
Naor <bdnaor@gmail.com>
2019-08-09 22:53:24 +0300

    return the delete place and add nevigator pop

--- src/screens/PlaceDetail/PlaceDetail.js
+++ src/screens/PlaceDetail/PlaceDetail.js
@@ -1,52 +1,71 @@
-import React from "react";
+import React, { Component } from "react";
 import { Modal, View, Image, Text, Button, StyleSheet, TouchableOpacity } from "react-native";
+import { connect } from 'react-redux';
 
 import Icon from 'react-native-vector-icons/Ionicons';
 
-const placeDetail = props => {
-  let modalContent = null;
+import { deletePlace } from '../../store/actions/index';
 
-  if (props.selectedPlace) {
-    modalContent = (
-      <View>
-        <Image source={props.selectedPlace.image} style={styles.placeImage} />
-        <Text style={styles.placeName}>{props.selectedPlace.name}</Text>
-      </View>
-    );
+class PlaceDetail extends Component {
+
+  onPlaceDeletedHandler = () => {
+    this.props.onItemDeleted(this.props.selectedPlace.key);
+    this.props.navigator.pop({
+      animated: true,
+      animationType: 'fade'
+    });
   }
-  return (
-    // <Modal
-    //   onRequestClose={props.onModalClosed}
-    //   visible={props.selectedPlace !== null}
-    //   animationType="slide"
-    // >
-      <View style={styles.modalContainer}>
-        {modalContent}
+
+  render(){
+    let modalContent = null;
+
+    if (this.props.selectedPlace) {
+      modalContent = (
         <View>
-          {/* <Button title="Delete" color="red" onPress={props.onItemDeleted} /> */}
-          <TouchableOpacity onPress={props.onItemDeleted}>
-            <Icon size={30} color="red" name="ios-trash" />
-          </TouchableOpacity>
-          {/* <Button title="Close" onPress={props.onModalClosed} /> */}
+          <Image source={this.props.selectedPlace.image} style={styles.placeImage} />
+          <Text style={styles.placeName}>{this.props.selectedPlace.name}</Text>
         </View>
-      </View>
-    // </Modal>
-  );
-};
-
-const styles = StyleSheet.create({
-  modalContainer: {
-    margin: 22
-  },
-  placeImage: {
-    width: "100%",
-    height: 200
-  },
-  placeName: {
-    fontWeight: "bold",
-    textAlign: "center",
-    fontSize: 28
-  }
+      );
+    }
+    return (
+      // <Modal
+      //   onRequestClose={props.onModalClosed}
+      //   visible={props.selectedPlace !== null}
+      //   animationType="slide"
+      // >
+        <View style={styles.modalContainer}>
+          {modalContent}
+          <View>
+            {/* <Button title="Delete" color="red" onPress={props.onItemDeleted} /> */}
+            <TouchableOpacity onPress={this.onPlaceDeletedHandler}>
+              <Icon size={30} color="red" name="ios-trash" />
+            </TouchableOpacity>
+            {/* <Button title="Close" onPress={props.onModalClosed} /> */}
+          </View>
+        </View>
+      // </Modal>
+    );
+  };
+}
+  const styles = StyleSheet.create({
+    modalContainer: {
+      margin: 22
+    },
+    placeImage: {
+      width: "100%",
+      height: 200
+    },
+    placeName: {
+      fontWeight: "bold",
+      textAlign: "center",
+      fontSize: 28
+    }
 });
 
-export default placeDetail;
+const mapDispatchToProps = dispatch => {
+  return {
+    onItemDeleted: key => dispatch(deletePlace(key))
+  }
+}
+
+export default connect(null, mapDispatchToProps)(PlaceDetail);
